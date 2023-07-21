70300ce54ccce7ff3b93bb4a131f5e592972ece3
david-lagrange <davidnlagrange@hotmail.com>
2023-07-21 13:04:23 -0500

    Changes to final version for test run

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import './App.css';
 function App() {
   return (
     <div className="min-h-screen flex flex-col items-center justify-center p-5 bg-gray-100">
-      <h1 className="text-3xl font-bold mb-10 text-center">Select your preferred event manager</h1>
+      <h1 className="text-3xl font-bold mb-10 text-center">Select an event manager to view our next meetups</h1>
       <div className="flex flex-col max-w-2xl w-full">
         <a href="https://www.meetup.com/milwaukee-meditation-dyads/" target="_blank" rel="noopener noreferrer" className="group">
           <div className="md:flex bg-white border-2 border-gray-200 rounded-lg shadow-lg mb-5 p-5 cursor-pointer hover:opacity-75 transition-opacity duration-200">
@@ -17,7 +17,7 @@ function App() {
             </div>
             <div className="md:w-3/4 md:pl-4 w-full">
               <h2 className="text-2xl font-bold mb-2 group-hover:underline">Milwaukee Meditations and Dyads - Meetup</h2>
-              <p className="text-gray-700">Description for the Meetup event goes here.</p>
+              <p className="text-gray-700">Meetup provides a platform that helps organize in-person events, making it easy to manage attendees, schedules, and event notifications.</p>
             </div>
           </div>
         </a>
@@ -32,7 +32,7 @@ function App() {
             </div>
             <div className="md:w-3/4 md:pl-4 w-full">
               <h2 className="text-2xl font-bold mb-2 group-hover:underline">Milwaukee Meditations and Dyads - Facebook</h2>
-              <p className="text-gray-700">Description for the Facebook event goes here.</p>
+              <p className="text-gray-700">Facebook Events enable the ability to view event details on your feed, and receive convenient updates and notifications for events.</p>
             </div>
           </div>
         </a>
